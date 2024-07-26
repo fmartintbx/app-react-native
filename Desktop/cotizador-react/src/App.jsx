fe96f27898b31d7aaab9a39c2d3da034f00310a1
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2024-07-26 01:10:16 -0300

    header

---
+++ Desktop/cotizador-react/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Header from "./components/Header"
+function App() {
+   //Antes del return cualquier codigo javascript
+
+
+
+  return (
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10"> 
+    
+      
+    </div>
+  )
+}
+
+export default App
